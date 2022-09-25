42d4fc700cf89045f812b95570659c78df236fdb
Charles Roth <charlieroth4@icloud.com>
2022-09-25 12:00:36 +0200

    variables: immutable variables
    example of double assign to immutable variables compiler error

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // immutable variable
+    let x = 5;
+    println!("The value of x is: {x}");
+    // compiler error: cannot assign twice to immutable variable
+    // x = 7;
+    // println!("The value of x is: {x}");
+}
