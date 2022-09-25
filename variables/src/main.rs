52aa11548f508a99f068435e2d4bd0b1f122c0b7
Charles Roth <charlieroth4@icloud.com>
2022-09-25 18:21:36 +0200

    variables: shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,18 @@
-const HOURS: u32 = 3;
-const SECONDS_IN_HOUR: u32 = 60 * 60;
-
 fn main() {
-    let seconds_in_three_hours = SECONDS_IN_HOUR * HOURS;
-    println!("There are {seconds_in_three_hours} seconds in {HOURS} hours");
+    // Shadowing is different from marking a variable as `mut`, because we'll
+    // get a compile-time error if we accidentally try to reassign to this
+    // reassign to this variable without using the `let` keyword
+    //
+    // The other difference between `mut` and shadowing is that because we're
+    // effectively creating a new variable when we use the `let` keyword again,
+    // we can change the type of the value but reuse the same name
+    let x = 5;
+    let x = x + 1;
+    {
+        // x == 8
+        let x = x + 2;
+        println!("x in the inner scope is: {x}")
+    }
+    // x == 6
+    println!("x is: {x}")
 }
