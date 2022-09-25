57c283012016373d6dcf0e5c646fa54907b72517
Charles Roth <charlieroth4@icloud.com>
2022-09-25 12:12:39 +0200

    variables: constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,7 @@
+const HOURS: u32 = 3;
+const SECONDS_IN_HOUR: u32 = 60 * 60;
+
 fn main() {
-    // mutable variable
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 7;
-    println!("The value of x is: {x}");
+    let seconds_in_three_hours = SECONDS_IN_HOUR * HOURS;
+    println!("There are {seconds_in_three_hours} seconds in {HOURS} hours");
 }
