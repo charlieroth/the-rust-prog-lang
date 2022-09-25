823bdca44c4b7f18e713ed979adf00a0431f5bf3
Charles Roth <charlieroth4@icloud.com>
2022-09-25 18:45:34 +0200

    functions: fn with return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,15 +1,13 @@
 fn main() {
-    // Expressions evaluate to a value
-    // 
-    // - Calling a function is an expression
-    // - Calling a macro is an expression
-    // - A new scope block with curly brackets is an expression
-
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("y = {y}");
+    let x = five();
+    let x = plus_one(x);
+    println!("x = {x}");
 }
 
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
