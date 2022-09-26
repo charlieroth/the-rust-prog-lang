166d7861d52db8fd5aef70917158358ee67346c4
Charles Roth <charlieroth4@icloud.com>
2022-09-26 21:15:48 +0200

    e&p: match on enum

--- enums_patt_match/src/main.rs
+++ enums_patt_match/src/main.rs
@@ -1,25 +1,39 @@
 #![allow(dead_code, unused_variables)]
 
-enum IpAddrKind {
-    V4,
-    V6,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+fn value_in_cents(c: Coin) -> u8 {
+    match c {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn print_value_in_cents(c: Coin) {
+    match c {
+        Coin::Penny => {
+            println!("A penny is worth 1 cent");
+        },
+        Coin::Nickel => {
+            println!("A nickel is worth 5 cents");
+        },
+        Coin::Dime => {
+            println!("A nickel is worth 10 cents");
+        },
+        Coin::Quarter => {
+            println!("A nickel is worth 25 cents");
+        },
+    }
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let c = Coin::Quarter;
+    print_value_in_cents(c);
 }
-
-fn route(ip_kind: IpAddrKind) {}
