fa2bd99da0549d4296cad64bd8d2fcca70a48ed8
Charles Roth <charlieroth4@icloud.com>
2022-09-26 21:21:49 +0200

    e&p: _ placeholder

--- enums_patt_match/src/main.rs
+++ enums_patt_match/src/main.rs
@@ -2,13 +2,13 @@
 
 fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
-fn move_player(num_spaces: u8) {}
+fn reroll() {}
 
 fn main() {
     let dice_roll = 8;
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        other => move_player(other),
+        _ => reroll(),
     }
 }
