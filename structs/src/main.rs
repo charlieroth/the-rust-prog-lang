c08199f88d650baf70041ea64a6f1255ee1edbe4
Charles Roth <charlieroth4@icloud.com>
2022-09-26 19:06:44 +0200

    structs: example program (2)

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    let w1 = 30;
-    let h1 = 50;
+    let rectangle = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels",
-        area(w1, h1)
+        area(rectangle)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    return width * height
+fn area(dimensions: (u32, u32)) -> u32 {
+    return dimensions.0 * dimensions.1;
 }
