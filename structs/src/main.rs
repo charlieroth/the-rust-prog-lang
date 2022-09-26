61e245949e2001d75ad436cdecacef2b84c64c7e
Charles Roth <charlieroth4@icloud.com>
2022-09-26 19:16:51 +0200

    structs: example program (6)
    Implement `can_fit(other: &Rectangle)` method for `Rectangle` struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,13 +8,22 @@ impl Rectangle {
     fn area(&self) -> u32 {
         return self.width * self.height;
     }
+
+    fn can_fit(&self, other: &Rectangle) -> bool {
+        return self.width > other.width && self.height > other.height;
+    }
 }
 
 fn main() {
-    let rectangle = Rectangle {
+    let r1 = Rectangle {
         width: 30,
         height: 50
     };
-
-    println!("area({:?}) = {}", rectangle, rectangle.area());
+    
+    let r2 = Rectangle {
+        width: 20,
+        height: 40
+    };
+    
+    println!("Can ({:?}) fit ({:?})? {}", r1, r2, r1.can_fit(&r2));
 }
