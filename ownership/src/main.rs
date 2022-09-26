fff4b026f46cca745d02543cad0758248b084691
Charles Roth <charlieroth4@icloud.com>
2022-09-26 09:21:43 +0200

    ownership: example of 'move' and copy

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,22 @@
 fn main() {
     let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1 = {}, s2 = {}", s1, s2);
+    takes_ownership(s1);
+
+    // Cannot use s1 here because prev function call
+    // moves ownership
+    // println!("s1 = {}", s1);
+    
+    let x = 42;
+    makes_copy(x);
+    println!("x = {}", x);
+}
+
+fn takes_ownership(some_str: String) {
+    println!("some_str = {}", some_str);
+    // `some_str` comes into scope and once function is about to return it calls
+    // `drop()` on `some_str`
+}
+
+fn makes_copy(some_int: i32) {
+    println!("some_int = {}", some_int);
 }
