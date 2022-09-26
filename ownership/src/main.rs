34270aa1bdcd30698e847b45cd6149824df515fd
Charles Roth <charlieroth4@icloud.com>
2022-09-26 10:16:09 +0200

    ownership: references example

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,20 +1,9 @@
 fn main() {
-    let s1 = gives_ownership();
-    println!("s1 = {}", s1);
-    let s2 = String::from("world");
-    let s3 = takes_and_gives(s2);
-
-    // compiler error: ownership moved to s3
-    // println!("s2 = {}", s2);
-
-    println!("s3 = {}", s3);
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("length of {} is {}", s1, len);
 }
 
-fn gives_ownership() -> String {
-    let s = String::from("hello");
-    return s;
-}
-
-fn takes_and_gives(s: String) -> String {
-    s
+fn calculate_length(s: &String) -> usize {
+    return s.len();
 }
