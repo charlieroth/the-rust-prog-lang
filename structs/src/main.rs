beb6c2dbb061d399d9bcdc7de7c3cd5c0b5db32d
Charles Roth <charlieroth4@icloud.com>
2022-09-26 19:03:40 +0200

    structs: tuple structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,37 +1,10 @@
-#[allow(dead_code)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64
-}
+struct Color(i32, i32, i32);
+
+struct Coordinate(i32, i32, i32);
 
 fn main() {
-    let charlie = User {
-        active: false,
-        email: String::from("charlie@email.com"),
-        username: String::from("charlie"),
-        sign_in_count: 0,
-    };
-
-    let charlie_active = User {
-        active: true,
-        username: String::from("charlie_active"),
-        ..charlie
-    };
-
-    let active_msg = if charlie.active { "is" } else { "is not" };
-    println!("{} {} active", charlie.username, active_msg);
-    
-    let active_msg = if charlie_active.active { "is" } else { "is not" };
-    println!("{} {} active", charlie_active.username, active_msg);
-
-    // Compiler Error: cannot use `charlie` because ownership was moved to
-    // `charlie_active` with `..` syntax
-    //
-    // let charlie_active = User {
-    //     active: true,
-    //     username: String::from("charlie_active"),
-    //     ..charlie
-    // };
+    let black = Color(0,0,0);
+    let origin = Coordinate(0,0,0);
+    println!("r value in black: {}", black.0);
+    println!("x value in origin: {}", origin.0);
 }
