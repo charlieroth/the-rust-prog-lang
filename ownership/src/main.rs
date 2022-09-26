ed62e9425a494fcbac96102f0dff63b60c42576e
Charles Roth <charlieroth4@icloud.com>
2022-09-26 11:05:35 +0200

    ownership: mutable reference example

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("length of {} is {}", s1, len);
+    // Create mutable variable
+    let mut s1 = String::from("hello");
+    println!("s1: {}", s1);
+    // Pass reference to s1 as a "mutable reference" to allow for mutating
+    // a reference
+    mutate(&mut s1);
+    println!("s1: {}", s1);
 }
 
-fn calculate_length(s: &String) -> usize {
-    return s.len();
+fn mutate(s: &mut String) {
+    return s.push_str(", world!");
 }
