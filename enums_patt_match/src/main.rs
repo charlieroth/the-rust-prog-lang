bcb74182bf72080a36247e8a48f654dde9e739fe
Charles Roth <charlieroth4@icloud.com>
2022-09-26 21:23:40 +0200

    e&p: control flow with if let

--- enums_patt_match/src/main.rs
+++ enums_patt_match/src/main.rs
@@ -1,14 +1,9 @@
 #![allow(dead_code, unused_variables)]
 
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
-fn reroll() {}
-
 fn main() {
-    let dice_roll = 8;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
-        _ => reroll(),
+    let config_max = Some(32);
+
+    if let Some(max) = config_max {
+        println!("The max is configured to be {}", max);
     }
 }
