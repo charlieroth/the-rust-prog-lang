561c6b8d094af122e169ddc79c1b1cf72b0c93cd
Charles Roth <charlieroth4@icloud.com>
2022-09-26 19:26:23 +0200

    structs: example program (7)
    Implement `square(size: u32)` associated function for `Rectangle` struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -12,6 +12,11 @@ impl Rectangle {
     fn can_fit(&self, other: &Rectangle) -> bool {
         return self.width > other.width && self.height > other.height;
     }
+
+    // Associated function (no reference to `self`)
+    fn square(size: u32) -> Self {
+        Self { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -19,11 +24,15 @@ fn main() {
         width: 30,
         height: 50
     };
+    println!("Area of ({:?}) is {}", r1, r1.area());
     
     let r2 = Rectangle {
         width: 20,
         height: 40
     };
-    
     println!("Can ({:?}) fit ({:?})? {}", r1, r2, r1.can_fit(&r2));
+
+    // Call associated functions with `::fn_name(..)` syntax
+    let s = Rectangle::square(10);
+    println!("Can ({:?}) fit ({:?})? {}", r1, s, r1.can_fit(&s));
 }
