418ca55cfc633196fb6b088b0acfc6493917270d
Charles Roth <charlieroth4@icloud.com>
2022-09-27 08:35:03 +0200

    gtl: example program (3)
    convert `fn largest(list: &[i32])` to `fn largest<T>(list: &[T])`

--- gen_trait_life/src/main.rs
+++ gen_trait_life/src/main.rs
@@ -1,9 +1,11 @@
-fn largest(list: &[i32]) -> &i32 {
+// Error due to lack of implementation of std::cmp::PartialEq
+// but this is how Generics are used in a function definition
+fn largest<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
     
-    for number in list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
@@ -12,10 +14,12 @@ fn largest(list: &[i32]) -> &i32 {
 
 fn main() {
     let list = vec![1, 2, 3, 4, 5];
+    // Notice there is no generic written in function call syntax
+    // Compiler will infer this typing
     let lrg = largest(&list);
     println!("The largest number is {}", lrg);
     
-    let list = vec![83, 32, 54, 2, 39];
+    let list = vec!['c','h','a','r','l','i','e'];
     let lrg = largest(&list);
     println!("The largest number is {}",lrg);
 }
