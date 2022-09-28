ba6f2069e7085666c17959449a06ce25a6d737d4
Charles Roth <charlieroth4@icloud.com>
2022-09-28 07:20:49 +0200

    collections: vectors

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("collections");
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    println!("v: {:?}", v);
+    
+    for i in &mut v {
+        *i += 10;
+    }
+    println!("v: {:?}", v);
+
+    let v2 = vec![1, 2, 3];
+    println!("v2: {:?}", v2);
+
+    for i in &v2 {
+        println!("{}", i);
+    }
 }
