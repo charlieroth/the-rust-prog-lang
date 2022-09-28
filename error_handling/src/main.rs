073c9da5627c23bd75de500377ea77129476c885
Charles Roth <charlieroth4@icloud.com>
2022-09-28 07:44:53 +0200

    errors: use unwrap_or_else method

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,17 +2,13 @@
 use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening file: {:?}", other_error)
-            }
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening file: {:?}", error)
         }
-    };
+    });
 }
