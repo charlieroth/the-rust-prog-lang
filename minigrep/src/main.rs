ccec7836d1655393fc33538c7d6d75dee89bd302
Charles Roth <charlieroth4@icloud.com>
2022-09-30 10:10:14 +0200

    minigrep: refactor run to return Result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,21 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        println!("Problem parsing arguments: {error}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
 struct Config {
     query: String,
@@ -7,21 +23,23 @@ struct Config {
 }
 
 impl Config {
-    fn from(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        return Config { query, file_path };
+        return Ok(Config { query, file_path });
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::from(&args);
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Failed to read file");
+// `Box<dyn Error>` is a type that implements the error trait
+// and is a pointer allocated on the heap
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // `?` operator will return the error value from the current
+    // function for the caller to handle
+    let contents = fs::read_to_string(config.file_path)?;
     println!("contents: {}", contents);
+    return Ok(());
 }
