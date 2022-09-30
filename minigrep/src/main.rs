b485aeb3196ffb76ac95aef4d840335f76e016ca
Charles Roth <charlieroth4@icloud.com>
2022-09-30 09:21:03 +0200

    minigrep: refactor to struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,23 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn from(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        return Config { query, file_path };
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let search_str = &args[1];
-    let file_path = &args[2];
-    let contents = fs::read_to_string(file_path)
+    let config = Config::from(&args);
+    let contents = fs::read_to_string(config.file_path)
         .expect("Failed to read file");
     println!("contents: {}", contents);
 }
