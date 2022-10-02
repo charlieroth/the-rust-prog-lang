e0da075d9794d51d809ebdef9fa1355df1067ff7
Charles Roth <charlieroth4@icloud.com>
2022-10-02 12:45:49 +0200

    functional: filter() method

--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,16 +1,13 @@
-// Iterators in Rust are implemented as a trait, `Iter<T>`
 fn main() {
-    let v = vec![1, 2, 3, 4];
+    let v = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    println!("{:?}", v);
 
-    // Iterators can be consumed by loops or other useful methods
-    // on the `Iter<T>` trait
-    //
-    // Calculate sum of `v` via `.reduce(..)`
-    // 
-    // Need to use `into_iter()` to allow for iteration over any of the moved
-    // values (`T`), immutable references, or mutable references
-    let sum = v.into_iter().reduce(|acc, num| {
-        return acc + num;
-    }).unwrap();
-    println!("sum: {}", sum);
+    let v_even = filter_evens(v);
+    println!("{:?}", v_even);
 } 
+
+fn filter_evens(v: Vec<i32>) -> Vec<i32> {
+    return v.into_iter().filter(|item| {
+        return item % 2 == 0;
+    }).collect();
+}
