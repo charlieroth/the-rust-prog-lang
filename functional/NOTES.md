253fc0d95ca7f45ebf593bd91c636b96fdc5a848
Charles Roth <charlieroth4@icloud.com>
2022-10-02 12:28:24 +0200

    functional: iterator consumption via loop

---
+++ functional/NOTES.md
@@ -0,0 +1,10 @@
+# Functional Language Features
+
+## Closures
+
+
+## Iterators
+
+In Rust iterators are lazy, meaning they have no effect until you call
+methods that consume the iterator
+
--- functional/src/main.rs
+++ functional/src/main.rs
@@ -1,3 +1,17 @@
+// Iterators in Rust are implemented as a trait, `Iter<T>`
 fn main() {
-    println!("Hello, world!");
-}
+    let v = vec![1, 2, 3, 4];
+
+    // Create an iterator Iter<i32> from Vec<i32>
+    let v_iter = v.iter();
+
+    // Iterators can be consumed by loops or other useful methods
+    // on the `Iter<T>` trait
+    //
+    // Calculate sum of `v` via a loop
+    let mut sum = 0;
+    for x in v_iter {
+        sum += x;
+    }
+    println!("sum: {}", sum); 
+} 
