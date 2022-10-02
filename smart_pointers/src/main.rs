e503645797bf75bdbb4e9a28bb1b4e00d9d569be
Charles Roth <charlieroth4@icloud.com>
2022-10-02 18:32:09 +0200

    smart-pointers: example showing ref count

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,23 +1,18 @@
+use std::rc::Rc;
+
 enum List {
     Cons(i32, Rc<List>),
     Nil,
 }
 
-enum List1 {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-use std::rc::Rc;
-
 fn main() {
     let a = Rc::new(List::Cons(5, Rc::new(List::Cons(10, Rc::new(List::Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = List::Cons(3, Rc::clone(&a));
-    let c = List::Cons(4, Rc::clone(&a));
-    
-    let a_1 = List1::Cons(5, Box::new(List1::Cons(10, Box::new(List1::Nil))));
-    // `a_1` is moved here
-    let b_1 = List1::Cons(3, Box::new(a_1));
-    // error: attempting to use borrowed value
-    let c_1 = List1::Cons(4, Box::new(a_1));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = List::Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
